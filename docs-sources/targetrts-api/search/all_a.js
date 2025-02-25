74e0214559b9925ca31244c2ebe04adcd5514037
jenkins <jenkins@hcl.com>
2025-02-25 07:26:38 +0000

    C++ TargetRTS generated doc. merge | build 81 | for commit: [9a478f5508cba4c68dedde26c7b2d9932b2cb886]

--- docs-sources/targetrts-api/search/all_a.js
+++ docs-sources/targetrts-api/search/all_a.js
@@ -1,8 +1,9 @@
 var searchData=
 [
   ['link_0',['Link',['../class_r_t_timer_list_1_1_link.html',1,'RTTimerList']]],
-  ['log_1',['Log',['../struct_log.html',1,'']]],
-  ['log_2',['log',['../class_log_1_1_base.html#a5865027dc1fdca9a69d909a52daf60ce',1,'Log::Base::log(const char *str)'],['../class_log_1_1_base.html#ab6005466dbce7671361fe7575b8ba1bb',1,'Log::Base::log(const RTString &amp;str)'],['../class_log_1_1_base.html#a3f181c7b5954377e729a547227939709',1,'Log::Base::log(const void *data, const RTObject_class *type)'],['../class_log_1_1_base.html#a4cda27fe68db222b839ae0331e11d7be',1,'Log::Base::log(const RTTypedValue &amp;)']]],
-  ['logmsg_3',['logMsg',['../class_r_t_actor.html#a67fc8760790c667bdbae0d2005738f4e',1,'RTActor']]],
-  ['lookup_4',['lookup',['../struct_r_t_object__class.html#ae172a3c9d360050394069d56c7fed29e',1,'RTObject_class']]]
+  ['lock_1',['lock',['../class_log_1_1_base.html#af7d4893677bab59cd71dac48b72eb473',1,'Log::Base::lock()'],['../struct_log.html#a8f3aae3e596fdda7c7565901bf577176',1,'Log::lock()']]],
+  ['log_2',['Log',['../struct_log.html',1,'']]],
+  ['log_3',['log',['../class_log_1_1_base.html#a5865027dc1fdca9a69d909a52daf60ce',1,'Log::Base::log(const char *str)'],['../class_log_1_1_base.html#ab6005466dbce7671361fe7575b8ba1bb',1,'Log::Base::log(const RTString &amp;str)'],['../class_log_1_1_base.html#a3f181c7b5954377e729a547227939709',1,'Log::Base::log(const void *data, const RTObject_class *type)'],['../class_log_1_1_base.html#a4cda27fe68db222b839ae0331e11d7be',1,'Log::Base::log(const RTTypedValue &amp;)']]],
+  ['logmsg_4',['logMsg',['../class_r_t_actor.html#a67fc8760790c667bdbae0d2005738f4e',1,'RTActor']]],
+  ['lookup_5',['lookup',['../struct_r_t_object__class.html#ae172a3c9d360050394069d56c7fed29e',1,'RTObject_class']]]
 ];
--- docs-sources/targetrts-api/search/functions_4.js
+++ docs-sources/targetrts-api/search/functions_4.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['enable_0',['enable',['../class_external_1_1_base.html#a20a21158ffc3d6872879f604b81f314c',1,'External::Base']]],
   ['encode_1',['encode',['../struct_r_t_object__class.html#a123c62181ab608814ddf14e1e769652b',1,'RTObject_class']]],
-  ['event_2',['event',['../class_external_1_1_base.html#ace2dda14b76b2e29ca006a697c4d132a',1,'External::Base']]]
+  ['endl_2',['endl',['../struct_log.html#a94c6e62a27d4b58746179919333fbf66',1,'Log']]],
+  ['event_3',['event',['../class_external_1_1_base.html#ace2dda14b76b2e29ca006a697c4d132a',1,'External::Base']]]
 ];
