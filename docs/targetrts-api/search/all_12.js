78074777ed698be0dae823c06cd72e9b583146b3
Sonia Mathew <sonia.mathew@prod.hclpnp.com>
2025-03-20 12:00:22 +0530

    Generated HTML for Release 2.0.6

--- docs/targetrts-api/search/all_12.js
+++ docs/targetrts-api/search/all_12.js
@@ -3,6 +3,7 @@ var searchData=
   ['unexpectedmessage_0',['unexpectedMessage',['../class_r_t_actor.html#ae43a356192a2f2dbb920d9252f5f872d',1,'RTActor']]],
   ['unexpectedstate_1',['unexpectedState',['../class_r_t_actor.html#a4ff70c41166a0ab5738eefa38541917f',1,'RTActor']]],
   ['unhandledmessage_2',['unhandledMessage',['../class_r_t_actor.html#a70e399ee02f601300ba838099c781608',1,'RTActor']]],
-  ['unused_3',['Unused',['../struct_r_t_import_list_1_1_unused.html',1,'RTImportList']]],
-  ['used_4',['Used',['../struct_r_t_import_list_1_1_used.html',1,'RTImportList']]]
+  ['unlock_3',['unlock',['../struct_log.html#acd6d350d7b70ac1e16416dec5a0e80db',1,'Log']]],
+  ['unused_4',['Unused',['../struct_r_t_import_list_1_1_unused.html',1,'RTImportList']]],
+  ['used_5',['Used',['../struct_r_t_import_list_1_1_used.html',1,'RTImportList']]]
 ];
--- docs/targetrts-api/search/functions_9.js
+++ docs/targetrts-api/search/functions_9.js
@@ -1,6 +1,7 @@
 var searchData=
 [
-  ['log_0',['log',['../class_log_1_1_base.html#a5865027dc1fdca9a69d909a52daf60ce',1,'Log::Base::log(const char *str)'],['../class_log_1_1_base.html#ab6005466dbce7671361fe7575b8ba1bb',1,'Log::Base::log(const RTString &amp;str)'],['../class_log_1_1_base.html#a3f181c7b5954377e729a547227939709',1,'Log::Base::log(const void *data, const RTObject_class *type)'],['../class_log_1_1_base.html#a4cda27fe68db222b839ae0331e11d7be',1,'Log::Base::log(const RTTypedValue &amp;)']]],
-  ['logmsg_1',['logMsg',['../class_r_t_actor.html#a67fc8760790c667bdbae0d2005738f4e',1,'RTActor']]],
-  ['lookup_2',['lookup',['../struct_r_t_object__class.html#ae172a3c9d360050394069d56c7fed29e',1,'RTObject_class']]]
+  ['lock_0',['lock',['../struct_log.html#a8f3aae3e596fdda7c7565901bf577176',1,'Log']]],
+  ['log_1',['log',['../class_log_1_1_base.html#a5865027dc1fdca9a69d909a52daf60ce',1,'Log::Base::log(const char *str)'],['../class_log_1_1_base.html#ab6005466dbce7671361fe7575b8ba1bb',1,'Log::Base::log(const RTString &amp;str)'],['../class_log_1_1_base.html#a3f181c7b5954377e729a547227939709',1,'Log::Base::log(const void *data, const RTObject_class *type)'],['../class_log_1_1_base.html#a4cda27fe68db222b839ae0331e11d7be',1,'Log::Base::log(const RTTypedValue &amp;)']]],
+  ['logmsg_2',['logMsg',['../class_r_t_actor.html#a67fc8760790c667bdbae0d2005738f4e',1,'RTActor']]],
+  ['lookup_3',['lookup',['../struct_r_t_object__class.html#ae172a3c9d360050394069d56c7fed29e',1,'RTObject_class']]]
 ];
